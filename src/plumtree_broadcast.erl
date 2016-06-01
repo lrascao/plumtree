71c86548ea7e85cc0b60ac1da1108130c4f797d2
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-06-01 16:22:54 -0400

    Add instrumentation

--- src/plumtree_broadcast.erl
+++ src/plumtree_broadcast.erl
@@ -256,12 +256,15 @@ handle_call({cancel_exchanges, WhichExchanges}, _From, State) ->
 -spec handle_cast(term(), #state{}) -> {noreply, #state{}}.
 handle_cast({broadcast, MessageId, Message, Mod}, State) ->
     {message_queue_len, MessageQueueLen} = process_info(self(), message_queue_len),
-    lager:info("One messaged processed; messages remaining: ~p",
+    lager:info("broadcast/3 messaged processed; messages remaining: ~p",
                [MessageQueueLen]),
     State1 = eager_push(MessageId, Message, Mod, State),
     State2 = schedule_lazy_push(MessageId, Mod, State1),
     {noreply, State2};
 handle_cast({broadcast, MessageId, Message, Mod, Round, Root, From}, State) ->
+    {message_queue_len, MessageQueueLen} = process_info(self(), message_queue_len),
+    lager:info("broadcast/6 messaged processed; messages remaining: ~p",
+               [MessageQueueLen]),
     Valid = Mod:merge(MessageId, Message),
     State1 = handle_broadcast(Valid, MessageId, Message, Mod, Round, Root, From, State),
     {noreply, State1};
