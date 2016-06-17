ebbd5fe1d55853091d1729be8ffa6dbca54a2a0c
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-06-17 20:17:01 +0200

    Add logging.

--- src/plumtree_broadcast.erl
+++ src/plumtree_broadcast.erl
@@ -479,7 +479,9 @@ random_root(#state{all_members=Members}) ->
 %% peer is not this node
 random_peer(Root, State=#state{all_members=All}) ->
     Eagers = all_eager_peers(Root, State),
+    lager:info("Eager: ~p", [Eagers]),
     Lazys  = all_lazy_peers(Root, State),
+    lager:info("Lazys: ~p", [Lazys]),
     Union  = ordsets:union([Eagers, Lazys]),
     Other  = ordsets:del_element(myself(), ordsets:subtract(All, Union)),
     case ordsets:size(Other) of
